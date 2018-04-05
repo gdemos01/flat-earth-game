fb2a0a6eae7d13d44925f43b77b72380a2cd8a73
gdemos01 <georgedemo2000@gmail.com>
2018-04-05 13:50:08 +0300

    Created Central Intelligence Script

---
+++ Flat Earth Game/Assets/Scripts/CentralIntelligence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CentralIntelligence : MonoBehaviour {
+
+	public GameObject agentPrefab;
+	public GameObject[] agents;
+
+	AgentFSM states;
+	private Vector3 destinationPoint;
+	private NavMeshAgent meshAgent;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (agents.Length < 150) 
+		{
+			Instantiate (agentPrefab, new Vector3 ((float)128.6, (float)0.83333,(float) 35.61394),Quaternion.identity);
+			agents = GameObject.FindGameObjectsWithTag ("EnemyAgent");
+		}
+
+		foreach (GameObject agent in agents)
+		{
+			states = agent.GetComponent<AgentFSM> ();
+			meshAgent = agent.GetComponent<NavMeshAgent>();
+			states.Walk();
+			destinationPoint = GameObject.Find ("Talf").GetComponent<Transform> ().position;
+			meshAgent.SetDestination (destinationPoint);
+		}
+	}
+}
