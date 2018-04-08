6da87625223027406c91be023f3138dbae674814
Kyriakos Kyriakou <kyriakosb4a@hotmail.com>
2018-04-08 13:54:20 +0300

    Glass Door Mechanism Created and Applied in Mayor's Office

---
+++ Flat Earth Game/Assets/Scripts/Doors.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour {
+
+    Animator animator;
+    bool doorOpen;
+
+	// Use this for initialization
+	void Start () {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            DoorAction("Open");
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorAction("Close");
+        }
+    }
+
+    void DoorAction(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
