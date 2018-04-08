12e33ccb7f2294a4d441a1e6f5697ed58e72853b
Kyriakos Kyriakou <kyriakosb4a@hotmail.com>
2018-04-08 16:31:32 +0300

    Animated Doors added to Municipality Scene

--- Flat Earth Game/Assets/Scripts/Door.cs
+++ Flat Earth Game/Assets/Scripts/Door.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Door : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    Animator animator;
+    bool doorOpen;
+
+    // Use this for initialization
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            animator.SetBool("open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            animator.SetBool("open", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
