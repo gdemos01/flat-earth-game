f468f94f1598b5ebeb091a855634c0c10e72e79b
gdemos01 <georgedemo2000@gmail.com>
2018-04-09 11:18:56 +0300

    Began adding citizens

---
+++ Flat Earth Game/Assets/Scripts/CitizenScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenScript : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		print("THISSSSSSSSSSSSSSSSSSSS: "+animator.GetBool ("Walk"));
+		animator.SetBool ("Walk", true);
+	}
+}
