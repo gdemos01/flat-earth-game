9c7b95b9be4ca5a79be62db548a2ab898814bb18
Kyriakos Kyriakou <kyriakosb4a@hotmail.com>
2018-04-09 03:02:16 +0300

    Dialog System Added and applied to Mrs. Mayor
    You can test it by moving near Mrs. Mayor and pressing E (the action
    button).
    
    If the character moves away Mrs. Mayor, the dialog box hides itself.

---
+++ Flat Earth Game/Assets/Scripts/NPC_Scripts/MrsMayorTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrsMayorTrigger : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+    /*AUTOMATIC ACTION
+     * 
+    private void OnTriggerEnter(Collider col)
+    {
+         if (col.gameObject.tag == "Player")
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+    }*/
+
+private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            FindObjectOfType<DialogueManager>().EndDialogue();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
