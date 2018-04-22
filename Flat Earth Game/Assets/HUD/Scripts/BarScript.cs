5cd81a39c9d83725ac3ba5a663a2d2b7b7ab5243
Kyriakos Kyriakou <kyriakosb4a@hotmail.com>
2018-04-22 18:10:04 +0300

    Creation of HUD Elements started

---
+++ Flat Earth Game/Assets/HUD/Scripts/BarScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+    private float fillAmount;
+
+    [SerializeField]
+    private Image fillIndicator;
+
+    public float MaxValue { get; set; }
+
+    public float Value
+    {
+        set
+        {
+            fillAmount = Map(value, 0, MaxValue, 0, 1);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleBar();
+	}
+
+    private void HandleBar()
+    {
+        if (fillAmount != fillIndicator.fillAmount)
+        {
+            fillIndicator.fillAmount = fillAmount;
+        }
+    }
+    
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
---
+++ Flat Earth Game/Assets/HUD/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stat health;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            health.CurrentVal -= 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            health.CurrentVal += 10;
+        }
+    }
+}
