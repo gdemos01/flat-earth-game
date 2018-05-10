a5f91e1988850335c1c969fe7a5ab7c69f8c5479
PAPA_G <gpapan03@cs.ucy.ac.cy>
2018-05-10 19:42:28 +0300

    minor changes in hq

--- Flat Earth Game/Assets/Scripts/link_test.cs
+++ Flat Earth Game/Assets/Scripts/link_test.cs
@@ -32,7 +32,7 @@ public class link_test : MonoBehaviour {
                 }
             if (door==2) {
                 SceneManager.LoadScene(2);
-            }
+                }
             }
         }
     }
---
+++ Flat Earth Game/Assets/inventory_woman_script.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory_woman_script : MonoBehaviour {
+
+    public const int maxHealth = 100;
+    public int currentHealth;
+
+    AudioSource _audioSource;
+    public AudioClip _audioClip;
+
+    // Use this for initialization
+    void Start () {
+
+        _audioSource = GetComponent<AudioSource>();
+        currentHealth = maxHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    public void TakeDamage(int amount, GameObject hit)
+    {
+        //_audioSource.PlayOneShot(hitsound);
+
+        currentHealth -= amount;
+        // print("health= " + currentHealth);
+        if (currentHealth <= 0)
+        {
+
+            Renderer[] renderers = GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in renderers)
+                r.enabled = false;
+            _audioSource.PlayOneShot(_audioClip);
+
+            Destroy(hit, _audioClip.length);
+        }
+    }
+}
