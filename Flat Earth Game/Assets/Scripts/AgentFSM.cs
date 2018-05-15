53ff465aab9eae92ffc5d8464205f4387212fb3a
PAPA_G <gpapan03@cs.ucy.ac.cy>
2018-05-15 21:39:00 +0300

    hq update + fit agent shoot

--- Flat Earth Game/Assets/Scripts/AgentFSM.cs
+++ Flat Earth Game/Assets/Scripts/AgentFSM.cs
@@ -2,95 +2,118 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AgentFSM : MonoBehaviour {
+public class AgentFSM : MonoBehaviour
+{
 
-	const int countOfDamageAnimations = 3;
-	int lastDamageAnimation = -1;
-	private Animator animator;
-	public GameObject bulletPrefab;
-	public Transform bulletSpawn;
+    const int countOfDamageAnimations = 3;
+    int lastDamageAnimation = -1;
+    private Animator animator;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
 
-	public void evaluateState(Vector3 target){
-		float distance = Vector3.Distance (target, transform.position);
-		if (distance > 50) {
-			Walk ();
-		}else if(distance > 1 && distance <= 50) {
-			Run ();
-			//print (distance);
-		} else {
-			// Panaxas comes here
-			// Add connections to your fighting scene here
-			// Feel free to use any other animations instead of attack() 
-			// Look at the entire script
-			// NOTE: ENEMIES SHOULD DIE EASYLY (MAYBE JUST A COUPLE OF TURTLE SHOTS				
-			transform.rotation = Quaternion.LookRotation (target);
-			Attack ();
-			/*
+    int shooti = 0;
 
-			// Shooting
-			GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position,Quaternion.LookRotation (target));
-			bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6f;
-			Vector3 v = bullet.GetComponent<Rigidbody>().velocity.normalized;   //used for calulating the range
+    public void evaluateState(Vector3 target)
+    {
+        float distance = Vector3.Distance(target, transform.position);
+        if (distance > 50)
+        {
+            Walk();
+        }
+        else if (distance > 1 && distance <= 50)
+        {
+            Run();
+            //print (distance);
+        }
+        else
+        {
+            // Panaxas comes here
+            // Add connections to your fighting scene here
+            // Feel free to use any other animations instead of attack() 
+            // Look at the entire script
+            // NOTE: ENEMIES SHOULD DIE EASYLY (MAYBE JUST A COUPLE OF TURTLE SHOTS				
+            transform.rotation = Quaternion.LookRotation(target);
+            Attack();
 
-			float t = 1 / ((Mathf.Abs(v.x) + Mathf.Abs(v.y) + Mathf.Abs(v.z)) / 3); //destroy bullet after some time
+            shooti++;
+            if (shooti - 100 >= System.Int32.MaxValue) { shooti = 0; }
+            if ((shooti % 20) == 0)
+            {
+                // Shooting
 
-			Destroy(bullet, t);*/
-		}
-	}
+                GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.LookRotation(target));
+                bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6f;
+                Vector3 v = bullet.GetComponent<Rigidbody>().velocity.normalized;   //used for calulating the range
 
-	void Awake () {
-		animator = GetComponent<Animator> ();
-	}
+                float t = 1 / ((Mathf.Abs(v.x) + Mathf.Abs(v.y) + Mathf.Abs(v.z)) / 3); //destroy bullet after some time
 
-	public void Stay () {
-		animator.SetBool("Aiming", false);
-		animator.SetFloat ("Speed", 0f);
-	}
+                Destroy(bullet, t);
+            }
+        }
+    }
 
-	public void Walk () {
-		animator.SetBool("Aiming", false);
-		animator.SetFloat ("Speed", 0.5f);
-	}
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
-	public void Run () {
-		animator.SetBool("Aiming", false);
-		animator.SetFloat ("Speed", 4f);
-	}
+    public void Stay()
+    {
+        animator.SetBool("Aiming", false);
+        animator.SetFloat("Speed", 0f);
+    }
 
-	public void Attack () {
-		Aiming ();
-		animator.SetTrigger ("Attack");
-	}
+    public void Walk()
+    {
+        animator.SetBool("Aiming", false);
+        animator.SetFloat("Speed", 0.5f);
+    }
 
-	public void Death () {
-		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Death"))
-			animator.Play("Idle", 0);
-		else
-			animator.SetTrigger ("Death");
-	}
+    public void Run()
+    {
+        animator.SetBool("Aiming", false);
+        animator.SetFloat("Speed", 4f);
+    }
 
-	public void Damage () {
-		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Death")) return;
-		int id = Random.Range(0, countOfDamageAnimations);
-		if (countOfDamageAnimations > 1)
-			while (id == lastDamageAnimation)
-				id = Random.Range(0, countOfDamageAnimations);
-		lastDamageAnimation = id;
-		animator.SetInteger ("DamageID", id);
-		animator.SetTrigger ("Damage");
-	}
+    public void Attack()
+    {
+        Aiming();
+        animator.SetTrigger("Attack");
+    }
 
-	public void Jump () {
-		animator.SetBool ("Squat", false);
-		animator.SetFloat ("Speed", 0f);
-		animator.SetBool("Aiming", false);
-		animator.SetTrigger ("Jump");
-	}
+    public void Death()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+            animator.Play("Idle", 0);
+        else
+            animator.SetTrigger("Death");
+    }
 
-	public void Aiming () {
-		animator.SetBool ("Squat", false);
-		animator.SetFloat ("Speed", 0f);
-		animator.SetBool("Aiming", true);
-	}
+    public void Damage()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death")) return;
+        int id = Random.Range(0, countOfDamageAnimations);
+        if (countOfDamageAnimations > 1)
+            while (id == lastDamageAnimation)
+                id = Random.Range(0, countOfDamageAnimations);
+        lastDamageAnimation = id;
+        animator.SetInteger("DamageID", id);
+        animator.SetTrigger("Damage");
+    }
+
+    public void Jump()
+    {
+        animator.SetBool("Squat", false);
+        animator.SetFloat("Speed", 0f);
+        animator.SetBool("Aiming", false);
+        animator.SetTrigger("Jump");
+    }
+
+    public void Aiming()
+    {
+        animator.SetBool("Squat", false);
+        animator.SetFloat("Speed", 0f);
+        animator.SetBool("Aiming", true);
+    }
 
 }
